621a06e2a86f8f30645363203d98f8fc41268c00
Rafael <rafaelribsantos16@gmail.com>
2026-04-08 12:30:46 -0400

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/header.jsx'
+import './css/index.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1>React App</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+
+
+function Header() {
+    return (
+        <header>
+            <img id="teste" src="/src/images/global-warming.png" alt="Global Warming" />
+            <h1>Aquecimento Global</h1>
+        </header>
+    )
+}
+
+export default Header
