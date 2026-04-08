8a5787bbb31c45a35b5413e1aad245dc5d6652b8
Rafael <rafaelribsantos16@gmail.com>
2026-04-08 14:35:29 -0400

    atualizando Header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,12 @@ function Header() {
     return (
         <header>
             <img id="teste" src="/src/images/global-warming.png" alt="Global Warming" />
-            <h1>Aquecimento Global</h1>
+            <li>
+                <ul>Home</ul>
+                <ul>Blog</ul>
+                <ul>Fale conosco</ul>
+                <ul>Conversor</ul>
+            </li>
         </header>
     )
 }
