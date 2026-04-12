6adaa3cc6025492abf09e141419ed2b1213820e5
RobsonRomualdo <164440613+RobsonRomualdo@users.noreply.github.com>
2026-04-12 12:36:57 -0400

    bug fix: removido testes do material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Header from './components/header.jsx'
 import './css/index.css'
-import Botao from './components/botao.jsx'
 
 function App() {
 
@@ -8,7 +7,6 @@ function App() {
     <>
       <Header />
       <h1>React App</h1>
-      <Botao />
     </>
   )
 }
