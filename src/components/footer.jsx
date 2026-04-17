21af1b53e7b9f3a02fee845dc88e07d8d0d74e34
Rafael Ribeiro <rafaelribsantos16@gmail.com>
2026-04-16 22:19:36 -0400

    criado Footer com MUI
    Foi criado o component Footer com AppBar do MUI, estilizado a centralização dos itens através do index.css para facilitar.
    Também adicionado box-shadow no componente Header

---
+++ src/components/footer.jsx
@@ -0,0 +1,32 @@
+import { Box, AppBar, Toolbar, Typography, Stack, IconButton } from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
+function Footer() {
+    return (
+        
+            <AppBar component="footer" position="fixed" sx={{width: '100%', height: '127px', top: 'auto', bottom: 0, backgroundColor: '#0F172A' }}>
+                <Box sx={{ display: "flex", alignItems: "center", gap: "8px", marginBottom: "6px"}}>
+                    <img src='/src/images/terra.png' alt="terra" />
+                    <Typography variant="h6" component="h6" sx={{ fontWeight: "700", fontSize: "16px"}}>
+                        Global Warming
+                    </Typography>
+                </Box>
+                <Typography variant="p" component="p" sx={{ fontWeight: "light", fontSize: "16px", marginBottom: "12px"}}>
+                    Promovendo a conscientização ambiental através da informação.
+                </Typography>
+                <Box sx={{ display: "flex", flexDirection: "column", flexDirection: "column", alignItems: "center"}}>
+                    <Typography variant="p" component="p" sx={{ fontWeight: "light", fontSize: "12px"}}>
+                        © 2024 Global Warming. 
+                    </Typography>
+                    <Typography variant="p" component="p" sx={{ fontWeight: "light", fontSize: "12px"}}>
+                        Todos os direitos reservados. Protótipo para Fins Educativos.
+                    </Typography>
+                </Box>
+            </AppBar>
+        
+    )
+}
+
+export default Footer
