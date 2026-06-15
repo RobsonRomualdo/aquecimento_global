af69d9da774e455df3549622ddc3d163541f31c9
mayoliveiraa <mayanaoliveira2003@gmail.com>
2026-06-15 16:42:50 -0400

    Finaliza estilização baseada no Figma
    Adiciona layout, cores, ícones, espaçamentos e estilos finais da página Fale Conosco.

--- src/pages/faleconosco.jsx
+++ src/pages/faleconosco.jsx
@@ -1,41 +1,78 @@
 import Header from "../components/header"
 import Footer from "../components/footer"
 import ADrawer from "../components/drawer"
+import "../css/faleconosco.css"
+import AButton from "../components/aButton"
+import { EnvelopeSimple, MapPin } from "phosphor-react"
 
 function FaleConosco() {
     return (
         <>
             <Header/>
 
-<main>
-    <h1>Contato</h1>
+            <main className="contato-container">
+                <section className="contato-box">
+                    <div className="contato-info">
+                        <h1>Contato</h1>
 
-    <p>
-        Tem alguma dúvida ou sugestão de conteúdo?
-        <br />
-        Entre em contato conosco.
-    </p>
+                        <p>
+                            Tem alguma dúvida ou sugestão de conteúdo?
+                            <br />
+                            Entre em contato conosco.
+                        </p>
 
-    <p>gwarming@gmail.com</p>
+                        <div className="contato-detalhes">
+                            <p>
+                                <div className="icone-contato">
+                                    <EnvelopeSimple size={19.5} weight="regular" />
+                                </div>
+                                gwarming@gmail.com
+                            </p>
 
-    <p>Brasil</p>
+                            <p>
+                                <div className="icone-contato">
+                                    <MapPin size={21} weight="regular" />
+                                </div>
+                                Brasil
+                            </p>
+                        </div>
+                    </div>
 
-    <form>
+                    <form className="contato-form">
+                        <div className="input-duplo">
+                            <div className="campo">
+                                <label>Seu nome</label>
+                                <input type="text" required />
+                            </div>
 
-        <label>Seu nome</label>
-        <input type="text" required />
+                            <div className="campo">
+                                <label>E-mail</label>
+                                <input type="email" required />
+                            </div>
+                        </div>
 
-        <label>E-mail</label>
-        <input type="email" required />
+                        <div className="campo">
+                            <label>Assunto</label>
+                            <input type="text" required />
+                        </div>
 
-        <label>Assunto</label>
-        <input type="text" required />
+                        <div className="campo">
+                            <label>Mensagem</label>
+                            <textarea required></textarea>
+                        </div>
 
-        <label>Mensagem</label>
-        <textarea required></textarea>
-
-    </form>
-</main>
+                        <AButton
+                            type="submit"
+                            texto="Enviar Mensagem"
+                            width="261px"
+                            height="48px"
+                            borderRadius="12px"
+                            fontSize="14px"
+                            fontWeight="600"
+                        />
+                    </form>
+                </section>
+            </main>
 
             <Footer/>
             <ADrawer/>
